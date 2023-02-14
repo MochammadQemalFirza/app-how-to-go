cb042987ca1bf3a9e17046304310392c6d7f838e
Mochammad Qemal Firza <kemalfirza@gmail.com>
2023-02-14 19:33:25 +0700

    new func added

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println(howtogo.Hello("Rudi"))
-	fmt.Println(howtogo.Profesi("Programmer"))
+	fmt.Println(howtogo.Hello(" Rudi"))
+	fmt.Println(howtogo.Profesi(" Programmer"))
+	fmt.Println(howtogo.Hobby(" Memancing"))
 }
